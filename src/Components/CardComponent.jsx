9d769cbe79707b11032c9a0c514b81ee57e8dccf
Arshjeevs <harshan4648@gmail.com>
2024-09-20 11:33:23 +0530

    add: Responsiveness to Components:

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import sofa from "../Images/sofa.png"
 const CardComponent = ({image}) => {
   return (
-    <div className='w-[400px] h-2/3 p-8'>
-        <img className='object-cover' src={image} alt="" />
-        <h1 className='text-4xl pt-4 pb-4'>Red Sofa</h1>
-        <button type="button" class="pt-text-gray-900 bg-white border border-black focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Buy Now</button>
-    </div>
+    <div className="w-full h-auto p-8 md:w-[400px] lg:h-2/3">
+    <img className="object-cover w-full" src={image} alt="" />
+    <h1 className="text-4xl pt-4 pb-4">Red Sofa</h1>
+    <button type="button" class="pt-text-gray-900 bg-white border border-black focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+        Buy Now
+    </button>
+</div>
   )
 }
 
--- src/Components/InputComponent.jsx
+++ src/Components/InputComponent.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const InputComponent = ({placeholder}) => {
   return (
     <div>
-        <input className={`bg-green-600 w-full py-4 px-6 border-none outline-none cursor text-white font-poppins rounded p-2 placeholder-white placeholder-opacity-60`} type="text" placeholder={placeholder}/>
+        <input
+              className={`bg-green-600 
+                          w-full lg:w-full xl:w-full md:w-full
+                          py-4 px-6 border-none outline-none cursor text-white font-poppins rounded p-2 placeholder-white placeholder-opacity-60`}
+              type="text"
+              placeholder={placeholder}
+            />
     </div>
   )
 }
